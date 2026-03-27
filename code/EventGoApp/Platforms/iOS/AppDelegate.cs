e3863dff0395fcde7ae83c008a10578236c0e4e6
P-S.Cypre <pscypre@gmail.com>
2026-03-27 17:49:59 -0400

    init MAUI

---
+++ code/EventGoApp/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace EventGoApp;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
